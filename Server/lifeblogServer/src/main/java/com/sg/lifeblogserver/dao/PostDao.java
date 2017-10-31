e176db1150a79a0130648626d3a09aa85dd54414
Asma Tabassum <asma.tabs@gmail.com>
2017-10-30 19:02:26 -0500

    Dao and models

---
+++ Server/lifeblogServer/src/main/java/com/sg/lifeblogserver/dao/PostDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.lifeblogserver.dao;
+
+import com.sg.lifeblogserver.model.Post;
+import java.util.List;
+
+/**
+ *
+ * @author asmat
+ */
+public interface PostDao {
+    
+    Post getById(long id);
+    List<Post> getAll();
+    Post add(Post post);
+    Post update(Post post);
+    void delete(long id);
+    
+}
---
+++ Server/lifeblogServer/src/main/java/com/sg/lifeblogserver/dao/PostDaoImpl.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.lifeblogserver.dao;
+
+import com.sg.lifeblogserver.model.Post;
+import java.util.List;
+
+/**
+ *
+ * @author asmat
+ */
+public class PostDaoImpl implements PostDao{
+
+    @Override
+    public Post getById(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Post> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Post add(Post post) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Post update(Post post) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
