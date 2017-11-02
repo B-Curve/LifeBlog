cd4e0e76f2ee79442d899a2199327d57c61f63a5
Asma Tabassum <asma.tabs@gmail.com>
2017-11-01 19:33:28 -0500

    Added dao methods

--- Server/lifeblogServer/src/main/java/com/sg/lifeblogserver/dao/CategoryDaoImpl.java
+++ Server/lifeblogServer/src/main/java/com/sg/lifeblogserver/dao/CategoryDaoImpl.java
@@ -6,12 +6,38 @@
 package com.sg.lifeblogserver.dao;
 
 import com.sg.lifeblogserver.model.Category;
+import java.util.List;
 
 /**
  *
  * @author asmat
  */
-public class CategoryDao {
+public class CategoryDaoImpl implements CategoryDao{
+
+    @Override
+    public List<Category> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Category getById() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Category add(Category category) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Category update(Category category) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Category category) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
-    Category category;
+    
 }
